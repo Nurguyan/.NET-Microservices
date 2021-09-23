d2f7d3a726f906f29c93a148451799cb5777743d
Nurguyan <centauri368@gmail.com>
2021-09-23 22:08:56 +0300

    command repository added

---
+++ CommandsService/AppDbContext/CommandRepo.cs
@@ -0,0 +1,66 @@
+﻿using CommandsService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.AppDbContext
+{
+    public class CommandRepo : ICommandRepo
+    {
+        private readonly AppDbContext _context;
+
+        public CommandRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            _context.Commands.Add(command);
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            _context.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId && c.Id == commandId).FirstOrDefault();
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return _context.Commands
+                .Where(c => c.Platform.Id == platformId)
+                .OrderBy(c => c.Platform.Name);
+        }
+
+        public bool PlatformsExists(int platformId)
+        {
+            return _context.Platforms.Any(p => p.Id == platformId);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ CommandsService/AppDbContext/ICommandRepo.cs
@@ -0,0 +1,24 @@
+﻿using CommandsService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.AppDbContext
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        // Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform platform);
+        bool PlatformsExists(int platformId);
+
+        //Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+
+    }
+}
